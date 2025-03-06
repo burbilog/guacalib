9927e61cd98175b49a21ac376727058ceafab20b
Roman V. Isaev <rm@aroma.ru>
2025-03-06 14:56:28 +0300

    fixed vars in guacalib/db_connection_parameters.py

--- guacalib/db_connection_parameters.py
+++ guacalib/db_connection_parameters.py
@@ -23,7 +23,7 @@ CONNECTION_PARAMETERS = {
         'table': 'connection'
     },
     'proxy_hostname': {
-        'type': 'int',
+        'type': 'string',
         'description': 'The hostname or IP address of the Guacamole proxy.',
         'default': 'NULL',
         'ref': 'https://guacamole.apache.org/doc/gug/jdbc-auth.html#connections-and-parameters',
@@ -209,17 +209,108 @@ CONNECTION_PARAMETERS = {
         'table': 'parameter'
     },
     'dest-host': {
-        'type': 'boolean',
+        'type': 'string',
         'description': 'The destination host to request when connecting to a VNC proxy.',
         'default': 'false',
         'ref': 'https://guacamole.apache.org/doc/gug/configuring-guacamole.html#vnc-repeater',
         'table': 'parameter'
     },
     'dest-port': {
-        'type': 'boolean',
+        'type': 'string',
         'description': 'The destination port to request when connecting to a VNC proxy.',
         'default': 'false',
         'ref': 'https://guacamole.apache.org/doc/gug/configuring-guacamole.html#vnc-repeater',
+        'table': 'parameter'
+    },
+    'disable-upload': {
+        'type': 'boolean',
+        'description': 'If set to true, uploads from the client (browser) to the remote server location will be disabled. ',
+        'default': 'false',
+        'ref': 'https://guacamole.apache.org/doc/gug/configuring-guacamole.html#device-redirection',
+        'table': 'parameter'
+    },
+    'disable-download': {
+        'type': 'boolean',
+        'description': 'If set to true downloads from the remote server to client (browser) will be disabled.',
+        'default': 'false',
+        'ref': 'https://guacamole.apache.org/doc/gug/configuring-guacamole.html#device-redirection',
+        'table': 'parameter'
+    },
+    'static-channels': {
+        'type': 'string',
+        'description': 'A comma-separated list of static channel names to open and expose as pipes. ',
+        'default': 'false',
+        'ref': 'https://guacamole.apache.org/doc/gug/configuring-guacamole.html#device-redirection',
+        'table': 'parameter'
+    },
+    'console-audio': {
+        'type': 'boolean',
+        'description': 'If set to “true”, audio will be explicitly enabled in the console (admin) session of the RDP server.',
+        'default': 'false',
+        'ref': 'https://guacamole.apache.org/doc/gug/configuring-guacamole.html#device-redirection',
+        'table': 'parameter'
+    },
+    'create-drive-path': {
+        'type': 'boolean',
+        'description': 'If set to “true”, and file transfer is enabled, the directory specified by the drive-path parameter will automatically be created if it does not yet exist.',
+        'default': 'false',
+        'ref': 'https://guacamole.apache.org/doc/gug/configuring-guacamole.html#device-redirection',
+        'table': 'parameter'
+    },
+    'drive-path': {
+        'type': 'string',
+        'description': 'The directory on the Guacamole server in which transferred files should be stored.',
+        'default': 'false',
+        'ref': 'https://guacamole.apache.org/doc/gug/configuring-guacamole.html#device-redirection',
+        'table': 'parameter'
+    },
+    'drive-name': {
+        'type': 'string',
+        'description': 'The name of the filesystem used when passed through to the RDP session.',
+        'default': 'false',
+        'ref': 'https://guacamole.apache.org/doc/gug/configuring-guacamole.html#device-redirection',
+        'table': 'parameter'
+    },
+    'enable-drive': {
+        'type': 'boolean',
+        'description': 'Enable file transfer support by setting this parameter to "true".',
+        'default': 'false',
+        'ref': 'https://guacamole.apache.org/doc/gug/configuring-guacamole.html#device-redirection',
+        'table': 'parameter'
+    },
+    'printer-name': {
+        'type': 'string',
+        'description': 'The name of the redirected printer device that is passed through to the RDP session.',
+        'default': 'false',
+        'ref': 'https://guacamole.apache.org/doc/gug/configuring-guacamole.html#device-redirection',
+        'table': 'parameter'
+    },
+    'enable-printing': {
+        'type': 'boolean',
+        'description': 'Enable printing by setting this parameter to "true".',
+        'default': 'false',
+        'ref': 'https://guacamole.apache.org/doc/gug/configuring-guacamole.html#device-redirection',
+        'table': 'parameter'
+    },
+    'enable-touch': {
+        'type': 'boolean',
+        'description': 'If set to "true", support for multi-touch events will be enabled',
+        'default': 'false',
+        'ref': 'https://guacamole.apache.org/doc/gug/configuring-guacamole.html#device-redirection',
+        'table': 'parameter'
+    },
+    'enable-audio-input': {
+        'type': 'boolean',
+        'description': 'If set to “true”, audio input support (microphone) will be enabled',
+        'default': 'false',
+        'ref': 'https://guacamole.apache.org/doc/gug/configuring-guacamole.html#device-redirection',
+        'table': 'parameter'
+    },
+    'disable-audio': {
+        'type': 'boolean',
+        'description': 'Disable sound by setting this parameter to "true"',
+        'default': 'false',
+        'ref': 'https://guacamole.apache.org/doc/gug/configuring-guacamole.html#device-redirection',
         'table': 'parameter'
     },
     'force-lossless': {
@@ -257,11 +348,109 @@ CONNECTION_PARAMETERS = {
         'ref': 'https://guacamole.apache.org/doc/gug/configuring-guacamole.html#display-settings',
         'table': 'parameter'
     },
+    'force-lossless': {
+        'type': 'boolean',
+        'description': 'Whether this connection should only use lossless compression for graphical updates. ',
+        'default': 'false',
+        'ref': 'https://guacamole.apache.org/doc/gug/configuring-guacamole.html#rdp-display-settings',
+        'table': 'parameter'
+    },  
+    'resize-method': {
+        'type': 'string',
+        'description': 'The method to use to update the RDP server when the width or height of the client display changes. ',
+        'default': 'false',
+        'ref': 'https://guacamole.apache.org/doc/gug/configuring-guacamole.html#rdp-display-settings',
+        'table': 'parameter'
+    },  
+    'dpi': {
+        'type': 'int',
+        'description': 'The desired effective resolution of the client display, in DPI.',
+        'default': 'false',
+        'ref': 'https://guacamole.apache.org/doc/gug/configuring-guacamole.html#rdp-display-settings',
+        'table': 'parameter'
+    },  
+    'height': {
+        'type': 'int',
+        'description': 'The height of the display to request, in pixels.',
+        'default': 'false',
+        'ref': 'https://guacamole.apache.org/doc/gug/configuring-guacamole.html#rdp-display-settings',
+        'table': 'parameter'
+    },  
+    'width': {
+        'type': 'int',
+        'description': 'The width of the display to request, in pixels.',
+        'default': 'false',
+        'ref': 'https://guacamole.apache.org/doc/gug/configuring-guacamole.html#rdp-display-settings',
+        'table': 'parameter'
+    },  
     'color-depth': {
         'type': 'int',
         'description': 'The color depth to request, in bits-per-pixel.',
         'default': 'false',
         'ref': 'https://guacamole.apache.org/doc/gug/configuring-guacamole.html#display-settings',
+        'table': 'parameter'
+    },  
+    'server-layout': {
+        'type': 'string',
+        'description': 'The server-side keyboard layout.',
+        'default': 'NULL',
+        'ref': 'https://guacamole.apache.org/doc/gug/configuring-guacamole.html#session-settings',
+        'table': 'parameter'
+    },
+    'initial-program': {
+        'type': 'string',
+        'description': 'The full path to the program to run immediately upon connecting.',
+        'default': 'NULL',
+        'ref': 'https://guacamole.apache.org/doc/gug/configuring-guacamole.html#session-settings',
+        'table': 'parameter'
+    },
+    'console': {
+        'type': 'boolean',
+        'description': 'If set to “true”, you will be connected to the console (admin) session of the RDP server.',
+        'default': 'NULL',
+        'ref': 'https://guacamole.apache.org/doc/gug/configuring-guacamole.html#session-settings',
+        'table': 'parameter'
+    },
+    'client-name': {
+        'type': 'string',
+        'description': 'If this parameter is specified, Guacamole will use its value as hostname.',
+        'default': 'NULL',
+        'ref': 'https://guacamole.apache.org/doc/gug/configuring-guacamole.html#session-settings',
+        'table': 'parameter'
+    },
+    'normalize-clipboard': {
+        'type': 'string',
+        'description': 'The type of line ending normalization to apply to text within the clipboard, if any.',
+        'default': 'NULL',
+        'ref': 'https://guacamole.apache.org/doc/gug/configuring-guacamole.html#clipboard-normalization',
+        'table': 'parameter'
+    },
+    'disable-auth': {
+        'type': 'string',
+        'description': 'If set to “true”, authentication will be disabled.',
+        'default': 'NULL',
+        'ref': 'https://guacamole.apache.org/doc/gug/configuring-guacamole.html#authentication-and-security',
+        'table': 'parameter'
+    },
+    'ignore-cert': {
+        'type': 'string',
+        'description': 'If set to “true”, the certificate returned by the server will be ignored.',
+        'default': 'NULL',
+        'ref': 'https://guacamole.apache.org/doc/gug/configuring-guacamole.html#authentication-and-security',
+        'table': 'parameter'
+    },
+    'security': {
+        'type': 'string',
+        'description': 'The security mode to use for the RDP connection: any|nla|nla-ext|tls|vmconnect|rdp',
+        'default': 'NULL',
+        'ref': 'https://guacamole.apache.org/doc/gug/configuring-guacamole.html#authentication-and-security',
+        'table': 'parameter'
+    },
+    'domain': {
+        'type': 'string',
+        'description': 'The domain to use when attempting authentication, if any. This parameter is optional',
+        'default': 'NULL',
+        'ref': 'https://guacamole.apache.org/doc/gug/configuring-guacamole.html#authentication-and-security',
         'table': 'parameter'
     }
 }
